c4fbf66c1c500b1c9854c624ab7a42565128cfd8
José Caldas <jrcaldas@yahoo.com.br>
2021-03-05 10:00:39 -0300

    initial commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function Container() {
+  return (
+    <Content>
+      <header>
+        <h1>Browse by interests</h1>
+      </header>
+    </Content>
+  );
+}
+
+const Content = styled.div``;
